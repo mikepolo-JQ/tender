0ff1edc1c60c64e17b2a11ffb8e432dfa1b0df74
Mikhail Palazkou <polozkov1450@gmail.com>
2021-07-28 20:52:39 +0300

    added store and review views & like review function

--- src/offer/utils.py
+++ src/offer/utils.py
@@ -11,8 +11,8 @@ class OfferFilter(filters.FilterSet):
     categories = CharFilterInFilter(field_name="categories__name", lookup_expr="in")
     types = CharFilterInFilter(field_name="types__name", lookup_expr="in")
     store = filters.CharFilter(field_name="store__name")
-    start_date = filters.DateFilter(lookup_expr='gte')
-    end_date = filters.DateFilter(lookup_expr='lte')
+    start_date = filters.DateFilter(lookup_expr="gte")
+    end_date = filters.DateFilter(lookup_expr="lte")
 
     class Meta:
         model = Offer
