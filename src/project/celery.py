80beb8e99f89e4da40a4766a2e48e8781dd96076
mikepolo_JQ <polozkov1450@gmail.com>
2021-07-29 18:28:44 +0300

    celery beat (update data from API every day at 00:00

--- src/project/celery.py
+++ src/project/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
@@ -9,3 +10,13 @@ app = Celery("project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+
+# Celery beat
+app.conf.beat_schedule = {
+    # Executes every day at 00:00
+    'update-data-every-day-at-midnight': {
+        'task': 'offer.tasks.update_data_file',
+        'schedule': crontab(minute=0, hour=0),
+    },
+}
--- src/project/settings.py
+++ src/project/settings.py
@@ -141,7 +141,7 @@ CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-REDIS_URL = os.environ.get('REDIS_URL', "redis://127.0.0.1:6379/0")
+REDIS_URL = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/0")
 
 CELERY_BROKER_URL = REDIS_URL
 CELERY_BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
